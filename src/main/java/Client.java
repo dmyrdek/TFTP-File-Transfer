b1ed64a40c281bbb23b5f9e673e3a74125f854e9
dmyrdek <dmyrdek@oswego.edu>
2019-04-06 15:52:05 -0400

    Working on client and server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class Client {
     private String ip;
@@ -14,15 +13,19 @@ public class Client {
     private DatagramPacket outputDatagramPacket;
     private DatagramPacket inputDatagramPacket;
 
-    public Client(String ip, int port) throws UnknownHostException {
+    public Client(String ip, int port) throws IOException {
         this.ip = ip;
         this.port = port;
         address = InetAddress.getByName(ip);
+        socket = new DatagramSocket();
     }
 
-    public void sendPacket(Packet packet) throws IOException {
-        socket = new DatagramSocket();
+    public byte[] sendPacket(Packet packet) throws IOException {
+        byte [] response = new byte [4];
         outputDatagramPacket = new DatagramPacket(packet.getByteArray(), packet.getByteArray().length, address, port);
-
+        socket.send(outputDatagramPacket);
+        DatagramPacket resp = new DatagramPacket(response, response.length);
+        socket.receive(resp);
+        return resp.getData();
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Server {
 
+    int port;
+    DatagramSocket socket;
+    byte [] message;
 
+    public Server(int port) {
+        this.port = port;
+        message = new byte[516];
+    }
+
+    public void startServer(Packet ackPacket) throws IOException {
+        DatagramPacket packet = new DatagramPacket(message, message.length);
+        socket = new DatagramSocket(port);
+        socket.receive(packet);
+        InetAddress address = packet.getAddress();
+        int port = packet.getPort();
+        packet = new DatagramPacket(ackPacket.getByteArray(), ackPacket.getByteArray().length, address, port);
+        socket.send(packet);
+    }
 }
