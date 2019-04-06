f0ba0769a2e171d8a7296c8ceb48a3f4c32e72b1
dmyrdek <dmyrdek@oswego.edu>
2019-04-06 15:21:49 -0400

    Fixed variable names in packet and working on client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,28 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Client {
+    private String ip;
+    private int port;
 
+    private DatagramSocket socket;
+    private InetAddress address;
 
+    private DatagramPacket outputDatagramPacket;
+    private DatagramPacket inputDatagramPacket;
 
+    public Client(String ip, int port) throws UnknownHostException {
+        this.ip = ip;
+        this.port = port;
+        address = InetAddress.getByName(ip);
+    }
+
+    public void sendPacket(Packet packet) throws IOException {
+        socket = new DatagramSocket();
+        outputDatagramPacket = new DatagramPacket(packet.getByteArray(), packet.getByteArray().length, address, port);
+
+    }
 }
--- src/main/java/Packet.java
+++ src/main/java/Packet.java
@@ -1,63 +1,49 @@
-import java.nio.ByteBuffer;
-
 public class Packet {
 
-    private ByteBuffer byteBuffer;
+    private byte[] byteArray;
+    private int byteLength = 0;
     private byte opCode;
     private byte block;
+    private byte zeroByte = 0;
 
     //Mode, always use octet
-    private static final String MODE = "octet";
+    private final String MODE = "octet";
 
     // Packet Size
-    private final static int PACKET_SIZE = 516;
+    private final int PACKET_SIZE = 516;
 
     //WRQ variables
     private String fileName;
-    private int wrqByteLength = 0;
-    private byte[] wrqByteArray;
-    private byte zeroByte = 0;
 
     //Data Variables
     private byte[] data;
-    private int dataByteLength = 0;
-    private byte[] dataByteArray;
 
-    //ACK Variables
-    private int ackByteLength = 0;
-    private byte[] ackByteArray;
-
-    //Error Variables
-    private int errorByteLength = 0;
-    private byte[] errorByteArray;
-
-    public ByteBuffer getByteBuffer() {
-        return byteBuffer;
+    public byte[] getByteArray(){
+        return byteArray;
     }
 
-    //WRQ Packet:
+    //WRQ Packet
     public Packet(byte opCode, String fileName){
         this.opCode = opCode;
         this.fileName = fileName;
-        wrqByteLength = 2 + fileName.length() + 1 + MODE.length() + 1;
-        wrqByteArray = new byte[wrqByteLength];
+        byteLength = 2 + fileName.length() + 1 + MODE.length() + 1;
+        byteArray = new byte[byteLength];
         int position = 0;
-        wrqByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
-        wrqByteArray[position] = opCode;
+        byteArray[position] = opCode;
         position++;
         for (int i = 0; i < fileName.length(); i++) {
-            wrqByteArray[position] = (byte) fileName.charAt(i);
+            byteArray[position] = (byte) fileName.charAt(i);
             position++;
         }
-        wrqByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
         for (int i = 0; i < MODE.length(); i++) {
-            wrqByteArray[position] = (byte) MODE.charAt(i);
+            byteArray[position] = (byte) MODE.charAt(i);
             position++;
         }
-        wrqByteArray[position] = zeroByte;
-        byteBuffer = ByteBuffer.wrap(wrqByteArray);
+        byteArray[position] = zeroByte;
     }
 
     //Data Packet
@@ -65,25 +51,24 @@ public class Packet {
         this.opCode = opCode;
         this.data = data;
 
-        dataByteLength = 2 + fileName.length() + 1 + MODE.length() + 1;
-        dataByteArray = new byte[dataByteLength];
+        byteLength = 2 + fileName.length() + 1 + MODE.length() + 1;
+        byteArray = new byte[byteLength];
         int position = 0;
 
-        dataByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
-        dataByteArray[position] = opCode;
+        byteArray[position] = opCode;
         position++;
-        dataByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
-        dataByteArray[position] = block;
+        byteArray[position] = block;
         position++;
         for (int i = 0; i < data.length; i++) {
-            dataByteArray[position] = data[i];
+            byteArray[position] = data[i];
 
             if (i != data.length-1)
                 position++;
         }
-        byteBuffer = ByteBuffer.wrap(dataByteArray);
     }
 
     //ACK Packet
@@ -91,19 +76,17 @@ public class Packet {
         this.opCode = opCode;
         this.block = block;
 
-        ackByteLength = 4;
-        ackByteArray = new byte[ackByteLength];
+        byteLength = 4;
+        byteArray = new byte[byteLength];
         int position = 0;
 
-        wrqByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
-        ackByteArray[position] = opCode;
+        byteArray[position] = opCode;
         position++;
-        ackByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
-        ackByteArray[position] = block;
-        byteBuffer = ByteBuffer.wrap(errorByteArray);
-
+        byteArray[position] = block;
     }
 
     //Error Packet
@@ -114,22 +97,21 @@ public class Packet {
         if (errorCode == 0)
             message = "Not defined, see error message (if any).";
 
-        errorByteLength = 2 + 2  + message.length() + 1;
-        errorByteArray = new byte[errorByteLength];
+        byteLength = 2 + 2  + message.length() + 1;
+        byteArray = new byte[byteLength];
         int position = 0;
-        wrqByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
-        errorByteArray[position] = opCode;
+        byteArray[position] = opCode;
         position++;
-        wrqByteArray[position] = zeroByte;
+        byteArray[position] = zeroByte;
         position++;
-        errorByteArray[position] = errorCode;
+        byteArray[position] = errorCode;
         position++;
         for (int i = 0; i < message.length(); i++) {
-            errorByteArray[position] = (byte) message.charAt(i);
+            byteArray[position] = (byte) message.charAt(i);
             position++;
         }
-        errorByteArray[position] = zeroByte;
-        byteBuffer = ByteBuffer.wrap(errorByteArray);
+        byteArray[position] = zeroByte;
     }
 }
